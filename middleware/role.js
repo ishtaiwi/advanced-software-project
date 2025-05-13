6261359ed1fb23bce07e275149db61b317cc6013
osama <osamaishtaiwi3@gmail.com>
2025-05-14 00:21:47 +0300

    delete role.js

--- middleware/role.js
+++
@@ -1,9 +0,0 @@
-// middleware/role.js
-module.exports = (...allowedRoles) => {
-  return (req, res, next) => {
-    if (!req.user || !allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({ msg: 'Access denied.' });
-    }
-    next();
-  };
-};
